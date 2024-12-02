fba9a4ba9b35f9881d807671894b77cb5674bcaa
Nils Imdahl <imdahlnils@gmail.com>
2024-12-02 18:16:07 +0100

    problem 2

--- 02/02_1.cpp
+++ 02/02_1.cpp
@@ -2,12 +2,25 @@
 #include <iostream>
 #include <sstream>
 #include <unordered_set>
+#include <vector>
+
+bool is_unsafe(int idx1, int idx2, bool inc) {
+  std::unordered_set<int> allowed_diffs = {1, 2, 3};
+  return (idx1 < idx2 != inc || !allowed_diffs.contains(std::abs(idx1 - idx2)));
+}
+
+bool check_safety(std::vector<int> vec) {
+  for (size_t i = 0; i < vec.size() - 1; ++i) {
+    if (is_unsafe(vec[i], vec[i + 1], vec[0] < vec[1]))
+      return false;
+  }
+  return true;
+}
 
 int main(int argc, char **argv) {
   std::ifstream f(argv[1]);
 
   std::string line;
-  std::unordered_set<int> allowed_diffs = {1, 2, 3};
   unsigned res = 0;
   while (std::getline(f, line)) {
     std::istringstream iss(line);
@@ -16,16 +29,7 @@ int main(int argc, char **argv) {
     while (iss >> number) {
       vec.push_back(number);
     }
-    bool inc = vec[0] < vec[1];
-    bool safe = true;
-    for (size_t i = 0; i < vec.size() - 1; ++i) {
-      if (vec[i] < vec[i + 1] != inc ||
-          !allowed_diffs.contains(std::abs(vec[i] - vec[i + 1]))) {
-        safe = false;
-        break;
-      }
-    }
-    res += safe;
+    res += check_safety(vec);
   }
   std::cout << res << std::endl;
   return 0;
--- 02/02_2.cpp
+++ 02/02_2.cpp
@@ -2,12 +2,35 @@
 #include <iostream>
 #include <sstream>
 #include <unordered_set>
+#include <vector>
+
+bool is_unsafe(int idx1, int idx2, bool inc) {
+  std::unordered_set<int> allowed_diffs = {1, 2, 3};
+  return (idx1 < idx2 != inc || !allowed_diffs.contains(std::abs(idx1 - idx2)));
+}
+
+int check_safety(std::vector<int> vec) {
+  for (size_t i = 0; i < vec.size() - 1; ++i) {
+    if (is_unsafe(vec[i], vec[i + 1], vec[0] < vec[1]))
+      return i;
+  }
+  return -1;
+}
+
+std::vector<int> remove_idx(std::vector<int> vec, int idx) {
+  idx = idx < 0 ? 0 : idx;
+  std::vector<int> res;
+  for (int i = 0; i < vec.size(); ++i) {
+    if (i != idx)
+      res.push_back(vec[i]);
+  }
+  return res;
+}
 
 int main(int argc, char **argv) {
   std::ifstream f(argv[1]);
 
   std::string line;
-  std::unordered_set<int> allowed_diffs = {1, 2, 3};
   unsigned res = 0;
   while (std::getline(f, line)) {
     std::istringstream iss(line);
@@ -16,17 +39,18 @@ int main(int argc, char **argv) {
     while (iss >> number) {
       vec.push_back(number);
     }
-    bool inc = vec[0] < vec[1];
-    unsigned safe = 2;
-    for (size_t i = 0; i < vec.size() - 1; ++i) {
-      if (vec[i] < vec[i + 1] != inc ||
-          !allowed_diffs.contains(std::abs(vec[i] - vec[i + 1]))) {
-        safe -= 1;
-      }
-      if (safe == 0)
+    bool safe = true;
+    for (size_t idx = 0; idx < vec.size() - 1; idx++) {
+      if (is_unsafe(vec[idx], vec[idx + 1], vec[0] < vec[1])) {
+        std::vector<int> low = remove_idx(vec, idx - 1);
+        std::vector<int> mid = remove_idx(vec, idx);
+        std::vector<int> high = remove_idx(vec, idx + 1);
+        safe = check_safety(low) == -1 || check_safety(mid) == -1 ||
+               check_safety(high) == -1;
         break;
+      }
     }
-    res += safe > 0 ? 1 : 0;
+    res += safe;
   }
   std::cout << res << std::endl;
   return 0;
